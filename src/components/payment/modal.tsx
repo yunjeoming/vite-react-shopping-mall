38ef48dfbbf598821468e0785fe9dee28e5abb22
yunjeoming <yunjeoming@gmail.com>
2023-01-11 22:48:23 +0900

    add modal css

--- src/components/payment/modal.tsx
+++ src/components/payment/modal.tsx
@@ -8,11 +8,13 @@ const ModalPortal = ({ children }: { children: ReactNode }) => {
 const PaymentModal = ({ show, proceed, cancel }: { show: boolean; proceed: () => void; cancel: () => void }) => {
   return show ? (
     <ModalPortal>
-      <div>
-        <p>정말 결제할까요?</p>
-        <div>
-          <button onClick={proceed}>예</button>
-          <button onClick={cancel}>아니오</button>
+      <div className={`modal ${show ? 'show' : ''}`}>
+        <div className="modal__inner">
+          <p>정말 결제할까요?</p>
+          <div>
+            <button onClick={proceed}>예</button>
+            <button onClick={cancel}>아니오</button>
+          </div>
         </div>
       </div>
     </ModalPortal>
--- src/pages/cart/itemData.tsx
+++ src/pages/cart/itemData.tsx
@@ -3,11 +3,11 @@ import { CartType } from '../../graphql/cart';
 
 const ItemData = ({ imageUrl, price, title }: Pick<CartType, 'imageUrl' | 'price' | 'title'>) => {
   return (
-    <li className="cart-item">
+    <div className="cart-item">
       <img className="cart-item__image" src={imageUrl} />
       <p className="cart-item__price">{price}</p>
       <p className="cart-item__title">{title}</p>
-    </li>
+    </div>
   );
 };
 
