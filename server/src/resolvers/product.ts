f2c5ed3a90b1a21c2003e614d849d17b7b8f3f43
yun <yunjeoming@gmail.com>
2023-01-20 16:57:37 +0900

    Add AdminPage and mutation Product

--- server/src/resolvers/product.ts
+++ server/src/resolvers/product.ts
@@ -1,11 +1,15 @@
 // schema에 정의한 것을 토대로 정의하는 곳 각각의 명령에 대해 적어주는 것
 
-import { Resolvers } from './types';
+import { Products, Resolvers } from './types';
+import { v4 as uuid } from 'uuid';
+import { DBField, writeDB } from '../dbController';
+
+const setJSON = (data: Products) => writeDB(DBField.PRODUCTS, data);
 
 const productResolver: Resolvers = {
   Query: {
     products: (parent, { cursor = '' }, { db }, info) => {
-      const fromIndex = db.products.findIndex(product => product.id === cursor) + 1;
+      const fromIndex = db.products.findIndex((product) => product.id === cursor) + 1;
       return db.products.slice(fromIndex, fromIndex + 15) || [];
     },
     product: (parent, { id }, { db }, info) => {
@@ -13,7 +17,50 @@ const productResolver: Resolvers = {
       return found || null;
     },
   },
-  Mutation: {},
+  Mutation: {
+    addProduct: (parent, { imageUrl, price, title, description }, { db }) => {
+      const newProduct = {
+        id: uuid(),
+        imageUrl,
+        price,
+        title,
+        description,
+        createdAt: Date.now(),
+      };
+
+      db.products.push(newProduct);
+      setJSON(db.products);
+      return newProduct;
+    },
+    updateProduct: (parent, { id, ...data }, { db }) => {
+      const existProductIndex = db.products.findIndex((item) => item.id === id);
+      if (existProductIndex < 0) {
+        throw new Error('없는 상품입니다.');
+      }
+      const updatedItem = {
+        ...db.products[existProductIndex],
+        ...data,
+      };
+      db.products.splice(existProductIndex, 1, updatedItem);
+      setJSON(db.products);
+      return updatedItem;
+    },
+    deleteProduct: (parent, { id }, { db }) => {
+      // 실제 db에서 delete를 하는 대신 flag만 걸어준다. -> createdAt을 지운다.
+      // sort, filter는 같은 옵션으로만 가능
+      const existProductIndex = db.products.findIndex((item) => item.id === id);
+      if (existProductIndex < 0) {
+        throw new Error('없는 상품입니다.');
+      }
+      const updatedItem = {
+        ...db.products[existProductIndex],
+      };
+      delete updatedItem.createdAt;
+      db.products.splice(existProductIndex, 1, updatedItem);
+      setJSON(db.products);
+      return id;
+    },
+  },
 };
 
 export default productResolver;
--- server/src/resolvers/types.ts
+++ server/src/resolvers/types.ts
@@ -21,7 +21,7 @@ export type Product = {
   price: number;
   title: string;
   description: string;
-  createdAt: number;
+  createdAt?: number;
 };
 
 export type Products = Product[];
